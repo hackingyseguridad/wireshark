4c5aba5b7cf8ed14955e29f6bdc8b2693a518621
hackingyseguridad <antonio_taboada@hotmail.es>
2022-06-01 13:20:51 +0200

    Update hex.sh

--- hex.sh
+++ hex.sh
@@ -1,5 +1,17 @@
-
-
+#!/bin/bash
 # Convierte texto en formato Hexadecimal
 # hackingyseguridad.com
+cat << "INFO"
+..
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Convierte texsto en formato hexadecimal"
+        echo "hackingyseguridad.com"
+        echo "Uso.: sh hex.sh <texto>"
+        echo
+        exit 0
+fi
+echo
+echo
 echo $1  | xxd  -c 256 -ps | sed "s/..$//"
